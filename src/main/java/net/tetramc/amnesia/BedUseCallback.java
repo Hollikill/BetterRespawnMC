8835b8f7fcf115762aa9ce306d34e5ff2be015b5
Hollikill <hollister.the.great@gmail.com>
2021-05-25 08:39:48 -0500

    Basic code implementation

---
+++ src/main/java/net/tetramc/amnesia/BedUseCallback.java
@@ -0,0 +1,22 @@
+package net.tetramc.amnesia;
+
+import net.fabricmc.fabric.api.event.Event;
+import net.fabricmc.fabric.api.event.EventFactory;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.ActionResult;
+
+public interface BedUseCallback {
+  public static final Event<BedUseCallback> EVENT = EventFactory.createArrayBacked(BedUseCallback.class, (listeners) -> (player) -> {
+            for (BedUseCallback listener : listeners) {
+                ActionResult result = listener.interact(player);
+ 
+                if(result != ActionResult.PASS) {
+                    return result;
+                }
+            }
+ 
+        return ActionResult.PASS;
+      });
+
+  ActionResult interact(PlayerEntity player);
+}
---
+++ src/main/java/net/tetramc/amnesia/mixin/BedUseMixin.java
@@ -0,0 +1,26 @@
+package net.tetramc.amnesia.mixin;
+
+import net.minecraft.block.BedBlock;
+import net.minecraft.state.State;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.tetramc.amnesia.BedUseCallback;
+import net.minecraft.entity.player.*;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin({BedBlock.class})
+public class BedUseMixin {
+  @Inject(at = {@At(value = "INVOKE", ordinal = 0)}, method = {"onUse"}, cancellable = true)
+  public void onSleep(State state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit, CallbackInfoReturnable<Boolean> info) {
+    ActionResult result = ((BedUseCallback)BedUseCallback.EVENT.invoker()).interact(player);
+    if (result == ActionResult.FAIL)
+      info.cancel(); 
+  }
+}
